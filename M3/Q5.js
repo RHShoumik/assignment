a7075b8bbf98c362fdd59453a7019c92524ca7ab
RHShoumik <rhshoumik12@gmail.com>
2023-06-20 21:56:24 +0600

    Q5 Done

---
+++ M3/Q5.js
@@ -0,0 +1,11 @@
+// 5.Navigator Object:
+
+
+// a. Write a JavaScript function that detects the user's browser name and version using the navigator object.
+let userInfo = () =>{
+    console.log("Browser name: " , navigator.appName);
+    console.log("Browser version: " , navigator.appVersion);
+}
+
+// b. Implement the function to display the user's browser name and version.
+userInfo();
