6109f60df546943990a5660206fff3e0234b0cc6
RHShoumik <rhshoumik12@gmail.com>
2023-06-20 22:24:08 +0600

    Q8 Done

---
+++ M3/Q6.js
@@ -0,0 +1,11 @@
+// a. Implement a JavaScript function that retrieves the user's current location (latitude and longitude) using the geolocation API.
+
+let userLocation = () =>{
+
+        const lat = position.coords.longitude;
+        const lng = position.coords.longitude;
+  
+        console.log(`Latitude: ${lat}, longitude: ${lng}`);
+}
+
+userLocation();
