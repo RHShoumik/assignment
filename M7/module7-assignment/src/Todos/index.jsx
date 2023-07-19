40192a8401a860642a81f5fdb6f6ad75c3b2bbd5
RHShoumik <rhshoumik12@gmail.com>
2023-07-20 00:15:27 +0600

    Assignment 7 Done With design

--- M7/module7-assignment/src/Todos/index.jsx
+++ M7/module7-assignment/src/Todos/index.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 import "./todos.scss";
 
 const Todos = () => {
-
   const [value, setValue] = useState("");
   const [todos, setTodos] = useState([
     {
@@ -26,26 +25,30 @@ const Todos = () => {
   ]);
 
   const addTodo = (e) => {
-    console.log(todos)
-    setTodos([...todos, {id: uuidv4(),text:value, isCompleted:false}]);
+    console.log(todos);
+    setTodos([...todos, { id: uuidv4(), text: value, isCompleted: false }]);
     setValue("");
   };
 
-  const deleteItem = (id) =>{
-    setTodos(todos.filter(item => item.id !== id));
-    console.log(todos)
-  }
+  const deleteItem = (id) => {
+    setTodos(todos.filter((item) => item.id !== id));
+    console.log(todos);
+  };
 
   return (
     <div className="Todos">
       <div className="Todos--input">
-            <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
-            <button onClick={addTodo}>Add</button>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
       </div>
 
       <div className="Todos--list">
         {todos.map((todo) => (
-          <div key={uuidv4()}>
+          <div className="Todos--list--single-item" key={uuidv4()}>
             <h3>{todo.text}</h3>
             <button onClick={() => deleteItem(todo.id)}>Delete</button>
           </div>
